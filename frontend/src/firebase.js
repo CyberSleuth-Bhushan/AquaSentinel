3affb95f7112d362ba59ecc9388a7e7d043876f4
CyberSleuth-Bhushan <bhushanmallick2006@gmail.com>
2026-02-23 14:06:40 +0530

    fix

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,14 +5,14 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD8xaJxgqa0zf5NQ3ERFBCRXOU3nCIr-c0",
-    authDomain: "tests-f8f05.firebaseapp.com",
-    projectId: "tests-f8f05",
-    storageBucket: "tests-f8f05.firebasestorage.app",
-    messagingSenderId: "477654984745",
-    appId: "1:477654984745:web:15bae0938b28421a5fd438",
-    measurementId: "G-P6FMJR6V56",
-    databaseURL: "https://tests-f8f05-default-rtdb.firebaseio.com/"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL
 };
 
 // Initialize Firebase
